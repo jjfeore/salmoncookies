bf40d56030a498f74caba47448b5b825f82d0c87
James Feore <jjfeore@gmail.com>
2017-03-13 10:55:05 -0700

    Javascript is largely done, just need to debug it

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var shopObj = {
   minCust: 0,
   maxCust: 0,
   avgCookies: 0,
+  pageElement: 0,
   randCust: function () {
-    return Math.floor(Math.random() * this.maxCust) + this.minCust;
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
   },
   cookieCount: [],
   dailySales: function () {
@@ -22,23 +23,44 @@ var pikeShop = new shopObj();
 pikeShop.minCust = 23;
 pikeShop.maxCust = 65;
 pikeShop.avgCookies = 6.3;
+pikeShop.pageElement = document.getElementById('pikeList');
+pikeShop.dailySales();
 
 var airportShop = new shopObj();
 airportShop.minCust = 3;
 airportShop.maxCust = 24;
 airportShop.avgCookies = 12;
+airportShop.pageElement = document.getElementById('airportList');
+airportShop.dailySales();
 
 var centerShop = new shopObj();
 centerShop.minCust = 11;
 centerShop.maxCust = 38;
 centerShop.avgCookies = 3.7;
+centerShop.pageElement = document.getElementById('centerList');
+centerShop.dailySales();
 
 var hillShop = new shopObj();
 hillShop.minCust = 20;
 hillShop.maxCust = 38;
 hillShop.avgCookies = 2.3;
+hillShop.pageElement = document.getElementById('hillList');
+hillShop.dailySales();
 
 var alkiShop = new shopObj();
 alkiShop.minCust = 2;
 alkiShop.maxCust = 16;
 alkiShop.avgCookies = 4.6;
+alkiShop.pageElement = document.getElementById('alkiList');
+alkiShop.dailySales();
+
+function listData(listObj) {
+  for (var a = 0; a < listObj.cookieCount.length; a++) {
+    listObj.pageElement.childNodes[a].firstChild.innerHTML = listObj.cookieCount[a];
+  }
+}
+listData(pikeShop);
+listData(airportShop);
+listData(centerShop);
+listData(hillShop);
+listData(alkiShop);
