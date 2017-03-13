3b8ee92c7227775cef2ce2bacfd044cc5cb31f72
James Feore <jjfeore@gmail.com>
2017-03-13 09:50:44 -0700

    Added the various shop objects

--- app.js
+++ app.js
@@ -17,3 +17,28 @@ var shopObj = {
     cookieCount.push(cookieTotal);
   }
 };
+
+var pikeShop = new shopObj();
+pikeShop.minCust = 23;
+pikeShop.maxCust = 65;
+pikeShop.avgCookies = 6.3;
+
+var airportShop = new shopObj();
+airportShop.minCust = 3;
+airportShop.maxCust = 24;
+airportShop.avgCookies = 12;
+
+var centerShop = new shopObj();
+centerShop.minCust = 11;
+centerShop.maxCust = 38;
+centerShop.avgCookies = 3.7;
+
+var hillShop = new shopObj();
+hillShop.minCust = 20;
+hillShop.maxCust = 38;
+hillShop.avgCookies = 2.3;
+
+var alkiShop = new shopObj();
+alkiShop.minCust = 2;
+alkiShop.maxCust = 16;
+alkiShop.avgCookies = 4.6;
