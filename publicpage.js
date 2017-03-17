3e15db6316dc48cc72dea28476a52aeaad8efd9f
James Feore <jjfeore@gmail.com>
2017-03-16 22:04:46 -0700

    Added slideshow. Now I just need a carousel for the merch.

--- publicpage.js
+++ publicpage.js
@@ -1,3 +1,22 @@
 'use strict';
 
-var pageBody = document.getElementsByTagName('body')[0];
+// Took this for an automatic slideshow from W3Schools and adapted it to include a rotating caption with the image
+
+var slideShowIndex = 0;
+slideShow();
+
+function slideShow() {
+  var imageList = document.getElementsByClassName('slideshow');
+  var capList = document.getElementsByClassName('about-cap');
+  for (var i = 0; i < imageList.length; i++) {
+    imageList[i].style.display = 'none';
+    capList[i].style.display = 'none';
+  }
+  if (slideShowIndex === imageList.length) {
+    slideShowIndex = 0;
+  }
+  imageList[slideShowIndex].style.display = 'block';
+  capList[slideShowIndex].style.display = 'block';
+  slideShowIndex++;
+  setTimeout(slideShow, 5000);
+}
